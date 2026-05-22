5a0de0309b8f57a32b0960add0dea276b2f9b4ae
yqz2025 <ye_qunzhong@163.com>
2026-05-22 16:38:32 +0800

    修改页面内容

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function clickButton(e: Event) {
+  console.log('点击了按钮');
+}
+function clickButton1(name: string, e: Event) {
+  console.log('点击了按钮', name, e);
+}
+function dblclickButton2(name: string, e: Event) {
+  console.log(name, e);
+}
+function rightclickButton3(name: string, e: Event) {
+  console.log("点击了按钮",name, e);
+}
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<div>
+  事件绑定
+  <div>点击事件<button @click="clickButton">点击我</button></div>
+  <div>点击事件<button @click="clickButton1('---单击', $event)">点击我</button></div>
+  <div>点击事件<button @dblclick="dblclickButton2('---双击', $event)">点击我</button></div>
+  <div>点击事件<button @click.right="rightclickButton3('---右击', $event)">点击我</button></div>
+</div>
 </template>
 
 <style scoped></style>
